cf57b7b0914736ebc0504e007d8a1b4a84367b44
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2022-05-13 20:35:19 +0600

    add / and /search routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Home';
+import Search from './Components/Search';
 
 function App() {
   return (
     <div>
-      App Placeholder
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
     </div>
   );
 }
